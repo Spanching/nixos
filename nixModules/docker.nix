7a8d57acf732edad81069a162407a13872c0ba79
Andreas <andreas@kruck.cc>
2025-12-21 19:52:21 +0100

    working nvidia docker state but not rootless

--- nixModules/docker.nix
+++ nixModules/docker.nix
@@ -1,12 +1,22 @@
 {config, pkgs, ... }:
 
 {
+  hardware.nvidia-container-toolkit.enable = true;
+
+  virtualisation.docker.package = pkgs.docker_25;
+  # virtualisation.docker.daemon.settings.features.cdi = true;
   virtualisation.docker = {
     enable = true;
-
-    rootless = {
-      enable = true;
-      setSocketVariable = true;
-    };
+    enableOnBoot = true;
+    # daemon = {
+    #   settings = {
+    #     default-runtime = "nvidia";
+    #   };
+    # };
+    # rootless = {
+    #   enable = true;
+    #   setSocketVariable = true;
+    #   daemon.settings.features.cdi = true;
+    # };
   };
 }
--- nixModules/nvidia.nix
+++ nixModules/nvidia.nix
@@ -5,14 +5,13 @@
   nixpkgs.config.allowUnfree = true;
 
   hardware.graphics.enable = true; # GL + Vulkan
+  # hardware.graphics.enable32Bit = true; # GL + Vulkan
 
   services.xserver.videoDrivers = [ "nvidia" ];
 
   hardware.nvidia = {
     modesetting.enable = true;
     open = true;
-    nvidiaSettings = true;
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+    nvidiaSettings = false;
   };
-  hardware.nvidia-container-toolkit.enable = true;
 }
